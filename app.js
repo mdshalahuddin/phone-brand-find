c9a0aad1d1c4202f3c930002fe2bd316d3a463cd
mdshalahuddin <mdshalahuddin004@gmail.com>
2022-03-01 23:29:50 +0600

    phone brand find

---
+++ app.js
@@ -0,0 +1,202 @@
+// Html Elements
+const header = document.getElementById('header').offsetHeight;
+const result = document.getElementById('result');
+const searchInput = document.getElementById('search-input');
+const showAllBtn = document.getElementById('show-all');
+const redAlertField = document.querySelector('.message');
+const searchBtn = document.getElementById('search-btn');
+
+// Fetch API
+const fetchAPI = async text => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${text}`;
+    const response = await fetch(url);
+    const results = await response.json();
+    const data = results.data;
+
+    if (response.ok && data.length !== 0) {
+        displayResult(data);
+        showAlert();
+    } else {
+        showAlert('No Result Found!');
+    }
+    spinner(false);
+}
+
+// Call Popular IPhones (Default)
+fetchAPI('iphone');
+
+// Display Results
+const displayResult = data => {
+
+    if (searching) {
+        removeAllChildNodes(result);
+    }
+
+    // All Phones Watch
+    data?.map((phone, index) => {
+        const { image, phone_name, brand, slug } = phone;
+
+        const div = document.createElement('div');
+        div.classList.add('col-lg-4', 'col-md-6');
+
+        div.innerHTML = `
+            <div class="card">
+                <figure onclick="loadDetails('${slug}')" data-bs-toggle="modal" data-bs-target="#phoneDetails">
+                    <img src="${image}" class="card-img-top" alt="${phone_name}" />
+                </figure>
+                <div class="card-body">
+                    <div class="d-flex align-items-center justify-content-between">
+                        <h5 onclick="loadDetails('${slug}')" class="card-title mb-0" data-bs-toggle="modal" data-bs-target="#phoneDetails">${phone_name}</h5>
+                        <h6 class="mb-0">Brand: ${brand}</h6>
+                    </div>
+                </div>
+            </div>
+        `;
+        result.appendChild(div);
+
+        if (index >= 12) {
+            div.classList.add('d-none');
+        }
+    });
+
+    // Display Show Button
+    if (data.length > 12) {
+        showAllBtn.classList.remove('d-none');
+    } else {
+        showAllBtn.classList.add('d-none');
+    }
+
+    if (searching) {
+        scrollDown();
+    }
+}
+
+// Load Phone Details (Function)
+const modalPreloader = document.querySelector('.preloader');
+const singleName = document.getElementById('name');
+const singleImage = document.getElementById('image');
+const singleBrand = document.getElementById('brand');
+const singleReleased = document.getElementById('released');
+const singleStorage = document.getElementById('storage');
+const singleDisplay = document.getElementById('display');
+const singleChipSet = document.getElementById('chipset');
+const singleMemory = document.getElementById('memory');
+const singleSensors = document.getElementById('sensors');
+const singleWlan = document.getElementById('wlan');
+const singleUsb = document.getElementById('usb');
+
+const loadDetails = async slug => {
+    // Show Preloader
+    modalPreloader.style.display = 'flex';
+    modalPreloader.style.opacity = '1';
+
+    // Fetch API
+    const url = `https://openapi.programming-hero.com/api/phone/${slug}`;
+    const response = await fetch(url);
+    const results = await response.json();
+    const data = results.data;
+
+    //Object Destructuring With default value for undefined Property
+    const {
+        name, image, brand, releaseDate,
+        mainFeatures: { storage, displaySize, chipSet, memory, sensors } = { storage: 'N/A', displaySize: 'N/A', chipSet: 'N/A', memory: 'N/A', sensors: 'N/A' },
+        others: { WLAN, USB } = { WLAN: 'N/A', USB: 'N/A' }
+    } = data;
+
+
+    const allSensors = sensors?.map(sensor => ' ' + sensor);
+
+    //  All Information phone
+    if (data) {
+        singleName.innerText = name;
+        singleImage.src = image;
+        singleBrand.innerText = brand;
+        singleReleased.innerText = releaseDate;
+        singleStorage.innerText = storage;
+        singleDisplay.innerText = displaySize;
+        singleChipSet.innerText = chipSet;
+        singleMemory.innerText = memory;
+        singleSensors.innerText = allSensors;
+        singleWlan.innerText = WLAN;
+        singleUsb.innerText = USB;
+    }
+
+    // Hide Preloader 
+    setTimeout(() => {
+        modalPreloader.style.opacity = '0';
+
+        setTimeout(() => {
+            modalPreloader.style.display = 'none';
+        }, 300);
+    }, 500);
+}
+
+// Search Phone
+let searching = false;
+searchBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+    const searchText = searchInput.value;
+    const trimSearch = searchText.trim();
+
+    if (trimSearch.length > 0) {
+        spinner(); // Show Spinner
+        fetchAPI(`${trimSearch}`);
+        searching = true;
+    } else {
+        showAlert('please give valid Input  Search!');
+    }
+    searchInput.value = '';
+});
+
+// Scroll Down to Results after Succesfull Search
+const scrollDown = () => {
+    window.scroll({
+        top: header,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
+
+// Remove Previous Results
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+// Spinner Function
+const spinner = (isShow = true) => {
+    if (isShow) {
+        searchBtn.innerHTML = `
+            <span class="spinner-load spinner-load-sm" role="status" aria-hidden="true"></span>
+            <span class="loading">Loading...</span>
+        `;
+        searchBtn.classList.add('disabled');
+    } else {
+        searchBtn.innerHTML = 'Search';
+        searchBtn.classList.remove('disabled');
+    }
+}
+
+// Alert Message Function
+const showAlert = (redAlert = '') => {
+    if (redAlert.length > 0) {
+        redAlertField.innerHTML = `<h6 class="d-inline-block m-0 bg-info text-danger  py-2 px-3 rounded">${redAlert}</h6>`;
+
+        // Remove Message After 3000 MS
+        setTimeout(() => {
+            redAlertField.innerHTML = '';
+        }, 3000);
+    } else {
+        redAlertField.innerHTML = '';
+    }
+}
+
+// Show All Hidden Elements
+showAllBtn.addEventListener('click', event => {
+    const hiddenElement = document.getElementsByClassName('d-none');
+    while (hiddenElement[0]) {
+        hiddenElement[0].classList.remove('d-none');
+    }
+    event.target.classList.add('d-none');
+});
